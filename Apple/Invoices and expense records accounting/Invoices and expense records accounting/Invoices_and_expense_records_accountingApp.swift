68525daa151d92862977248927ffa802020b783f
weachang chiau <yuyu11iiuu@gmail.com>
2026-08-11 19:38:12 +0800

    cwc
    Change

---
+++ Apple/Invoices and expense records accounting/Invoices and expense records accounting/Invoices_and_expense_records_accountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Invoices_and_expense_records_accountingApp.swift
+//  Invoices and expense records accounting
+//
+//  Created by Chaiau on 2026/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Invoices_and_expense_records_accountingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
