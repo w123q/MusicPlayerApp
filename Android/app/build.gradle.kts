9f659e68608f6b2d334ea0c37b52bacb732df05c
w123q <yuyu11iiuu@gmail.com>
2026-09-24 12:03:39 +0800

    Craatce add

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.packingtraavelchecklist"
-
-    // 注意：如果是標準 DSL，通常直接寫 compileSdk = 37
-    compileSdk = 37
+    namespace = "com.example.simplemusicplayer"
+    compileSdk {
+        version = release(37)
+    }
 
     defaultConfig {
-        applicationId = "com.example.packingtraavelchecklist"
+        applicationId = "com.example.simplemusicplayer"
         minSdk = 24
         targetSdk = 37
         versionCode = 1
@@ -21,15 +21,9 @@ android {
 
     buildTypes {
         release {
-            // 開啟程式碼混淆與壓縮（減少 APK 體積與垃圾）
-            isMinifyEnabled = true
-            // 開啟自動移除未使用的資源檔
-            isShrinkResources = true
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            optimization {
+                enable = false
+            }
         }
     }
     compileOptions {
@@ -43,28 +37,20 @@ android {
 
 dependencies {
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.activity.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.fragment)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.recyclerview)
-    implementation(libs.material)
-
     testImplementation(libs.junit)
-
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
-
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
--- Android/settings.gradle.kts
+++ Android/settings.gradle.kts
@@ -22,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Packing &Traavel Checklist"
+rootProject.name = "SimpleMusicPlayer"
 include(":app")
